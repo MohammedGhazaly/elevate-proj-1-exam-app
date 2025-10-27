95aa9bf56082d2abfd8cc06369fff85eec290fd6
Ahmed Alam <ahmedalam4887482@gmail.com>
2025-10-27 17:52:36 +0300

    feat: Implement initial structure for Forget Password feature with necessary files and classes

---
+++ lib/features/forget_paswword/presentation/view/pages/forget_paswword_page.dart
@@ -0,0 +1,10 @@
+import 'package:exam_app/features/forget_paswword/presentation/view/widgets/forget_paswword_body.dart';
+import 'package:flutter/material.dart';
+
+class ForgetPaswwordPage extends StatelessWidget {
+  const ForgetPaswwordPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: ForgetPaswwordBody());
+  }
+}
